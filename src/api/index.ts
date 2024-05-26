a792cd37ee503aca9489f76551cb031d0a7fe086
aoyuru <102245971+1Aost@users.noreply.github.com>
2024-05-26 19:14:45 +0800

    refactor:登录注册接口重构

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,59 +1,46 @@
 // 引入刚才的http.js文件
-import https from './request';	
+import https from './request';
 // 设置个对象，就不用一个个暴露了，直接暴露对象
 interface MyObject {
 	[key: string]: any;
 }
-interface UserMessage {
-	username: string
-	password: string
-}
-let apiFun :MyObject={};
-/* 登录 */
-apiFun.login=function(params: UserMessage) {
-	return https.post("/api/token",params);
-}
-/* 注册 */
-apiFun.register=function(params: UserMessage) {
-	return https.post("/api/register",params);
-}
-
+const apiFun: MyObject = {};
 
 /* 获取文章列表 */
-apiFun.getBlogList=function() {
+apiFun.getBlogList = function () {
 	return https.get("/api/articles/all");
 }
 /* 获取指定类型type的文章 */
-apiFun.getBlogByType=function(params:{type: number}) {
-	return https.get("/api/articles/types",params);
+apiFun.getBlogByType = function (params: { type: number }) {
+	return https.get("/api/articles/types", params);
 }
 /* 获取指定id的文章并同时修改访问人数 */
-apiFun.getBlog1=function(params: {id: number}) {
-	return https.get("/api/articles/some",params);
+apiFun.getBlog1 = function (params: { id: number }) {
+	return https.get("/api/articles/some", params);
 }
 /* 根据指定的id修改文章的点赞人数 */
-apiFun.changeLikes=function(params:any) {
-	return https.get("/api/articles/likes",params);
+apiFun.changeLikes = function (params: any) {
+	return https.get("/api/articles/likes", params);
 }
 /* 根据文章的id获取所有评论 */
-apiFun.getComments=function(params: {id: number}) {
-	return https.get("/api/articles/comments",params);
+apiFun.getComments = function (params: { id: number }) {
+	return https.get("/api/articles/comments", params);
 }
 /* 根据文章的id以及用户id提交文章评论 */
-apiFun.submitComments=function(params: {article_id: number,token: string,comments: string}) {
-	return https.post("/api/articles/newcomments",params);
+apiFun.submitComments = function (params: { article_id: number, token: string, comments: string }) {
+	return https.post("/api/articles/newcomments", params);
 }
 /* 获取所有的分类 */
-apiFun.getAllTypes=function() {
+apiFun.getAllTypes = function () {
 	return https.get("/api/types/all");
 }
 /* 根据用户id获取评论 */
-apiFun.getCommentsById=function(params: {id: number}) {
-	return https.get("/api/articles/commentsbyid",params);
+apiFun.getCommentsById = function (params: { id: number }) {
+	return https.get("/api/articles/commentsbyid", params);
 }
 /* 根据id删除评论 */
-apiFun.deleteComments=function(params: {comments_id: number}) {
-	return https.get("/api/articles/nocomment",params);
+apiFun.deleteComments = function (params: { comments_id: number }) {
+	return https.get("/api/articles/nocomment", params);
 }
 interface SubmitReviewType {
 	review_message: string
@@ -62,24 +49,24 @@ interface SubmitReviewType {
 }
 
 /* 获取所有留言 */
-apiFun.getReviews=function() {
+apiFun.getReviews = function () {
 	return https.get("/api/reviews");
 }
 /* 根据id获取留言 */
-apiFun.getReviewsById=function(params: {id: number}) {
-	return https.get("/api/reviews/reviewsbyid",params);
+apiFun.getReviewsById = function (params: { id: number }) {
+	return https.get("/api/reviews/reviewsbyid", params);
 }
 /* 根据id删除留言 */
-apiFun.deleteReviews=function(params: {id: number}) {
-	return https.get("/api/reviews/noreview",params);
+apiFun.deleteReviews = function (params: { id: number }) {
+	return https.get("/api/reviews/noreview", params);
 }
 /* 提交留言 */
-apiFun.submitReview=function(params: SubmitReviewType) {
-	return https.post("/api/reviews/newreview",params);
+apiFun.submitReview = function (params: SubmitReviewType) {
+	return https.post("/api/reviews/newreview", params);
 }
 /* 根据token获取用户信息 */
-apiFun.getUserByToken=function(params: {token: string}) {
-	return https.post("/users/bytoken",params);
+apiFun.getUserByToken = function (params: { token: string }) {
+	return https.post("/users/bytoken", params);
 }
 interface UserType {
 	id: number
@@ -88,29 +75,29 @@ interface UserType {
 	avatar: string
 }
 /* 修改用户 */
-apiFun.changeUser=function(params: UserType) {
-	return https.post("/users/oldtonew",params);
+apiFun.changeUser = function (params: UserType) {
+	return https.post("/users/oldtonew", params);
 }
 /* 上传头像 */
-apiFun.uploadAvatar=function(params: FormData) {
-	return https.postImg("/api/Upload/avatar",params)
+apiFun.uploadAvatar = function (params: FormData) {
+	return https.postImg("/api/Upload/avatar", params)
 }
 /* 判断token是否失效 */
-apiFun.LegalToken=function(params: string) {
-	return https.post("/users/token",params);
+apiFun.LegalToken = function (params: string) {
+	return https.post("/users/token", params);
 }
 
-apiFun.submitLikes=function(params: {article_id: number,user_id: number}) {
-	return https.post("/api/likes/new",params);
+apiFun.submitLikes = function (params: { article_id: number, user_id: number }) {
+	return https.post("/api/likes/new", params);
 }
-apiFun.selectLikes=function(params: {article_id: number,user_id: number}) {
-	return https.post("/api/likes/search",params);
+apiFun.selectLikes = function (params: { article_id: number, user_id: number }) {
+	return https.post("/api/likes/search", params);
 }
-apiFun.deleteLikes=function(params: {article_id: number,user_id: number}) {
-	return https.post("/api/likes/no",params);
+apiFun.deleteLikes = function (params: { article_id: number, user_id: number }) {
+	return https.post("/api/likes/no", params);
 }
-apiFun.selectLikesByUserId=function(params: {id: number}) {
-	return https.post("/api/likes/searchbyuserid",params);
+apiFun.selectLikesByUserId = function (params: { id: number }) {
+	return https.post("/api/likes/searchbyuserid", params);
 }
 //暴露出这个对象
 export default apiFun;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,75 +1,63 @@
-import React, { ChangeEvent, useState } from 'react'
-import { NavLink,useNavigate } from 'react-router-dom'
+import React, { useState } from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
 import { SmileOutlined, KeyOutlined } from "@ant-design/icons"
 import { message } from 'antd'
 import { useDispatch } from 'react-redux'
-import {change} from "../../store/reducers/userSlice"
-import apiFun from '../../api'
+import { change } from "../../store/reducers/userSlice"
 import "../../assets/font_style_cn.css"
 import "./index.css"
-interface MyMessageType {
-  code: string // 返回的状态码
-  msg: string // 提示信息
-  data: string // 携带的token
-}
-const Login:React.FC=()=>{
-  const [username,setUsername]=useState<string>("");
-  const [password,setPassword]=useState<string>("");
-  const [info,setInfo]=useState<string>("");
-  const navigate=useNavigate();
-  const dispatch=useDispatch();
-  function handleLogin():void {
-    if(username.trim().length<2 || username.trim().length>5) {
+import { login } from '../../services/Login'
+
+const Login = () => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [info, setInfo] = useState<string>("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleLogin = () => {
+    if (username.trim().length < 2 || username.trim().length > 5) {
       setInfo("用户名的长度不能小于2位大于5位");
-    }else if(password.trim().length<7 || password.trim().length>17) {
+    } else if (password.trim().length < 7 || password.trim().length > 17) {
       setInfo("密码的长度不能小于7位大于17位");
-    }else {
-      (async function() {
-        try {
-          const res:MyMessageType=await apiFun.login({username:username.trim(),password:password.trim()});
-          if(res.code==='0000') {
-            localStorage.setItem("token",res.data);
-            message.success("成功登录，欢迎欢迎");
-            dispatch(change(true));
-            navigate("/home");
-          }else {
-            message.warning(res.msg);
-          }
-        }catch(err) {
-          message.error("出错了，请联系管理员");
+    } else {
+      login({ username, password }).then(res => {
+        console.log(res);
+        if (res.code === '0000') {
+          localStorage.setItem("token", res.data);
+          message.success("成功登录，欢迎欢迎");
+          dispatch(change(true));
+          navigate("/home");
+        } else {
+          message.warning(res.msg);
         }
-      })()
+      }).catch(_err => {
+        message.error("出错了，请联系管理员");
+      })
     }
-  }
-  function handleUsername(e: ChangeEvent<HTMLInputElement>): void {
-    setUsername(e.target.value)
-  }
-  function handlePassword(e: ChangeEvent<HTMLInputElement>): void {
-    setPassword(e.target.value)
   }
   return (
     <main>
-        <div className="login">
-            <div className="login-left">
-                <div className="login-ltcmt"></div>
-                <h1 className="font-style">没有账号</h1>
-                <p className="font-style">您可以通过下方点击注册按钮，拥有Aoblog账号</p>
-                <NavLink className="font-style" to="/register">注册</NavLink>
-            </div>
-            <div className="login-right">
-            <h1 className="text-style-1 font-style">欢迎来到Aoblog</h1>
-            <i className="Rectangle-2"></i>
-            <p className="text_style_2 font-style">欢迎来到Aoblog！</p>
-            <div id="login">
-                <i className="user"><SmileOutlined style={{fontSize:"27px",color:"#666"}} /></i>
-                <input type="text" name={username} onChange={handleUsername} className="input" placeholder="请输入您的用户名" id="hs_user"/>
-                <i className="password"><KeyOutlined style={{fontSize:"27px",color:"#666"}} /></i>
-                <input type="password" name={password} onChange={handlePassword} className="input" placeholder="请输入您的密码" id="hs_psw"/>
-                <div className="info font-style">{info}</div>
-                <button onClick={handleLogin} className="btn font-style">登录</button>
-            </div>
-            </div>
+      <div className="login">
+        <div className="login-left">
+          <div className="login-ltcmt"></div>
+          <h1 className="font-style">没有账号</h1>
+          <p className="font-style">您可以通过下方点击注册按钮，拥有Aoblog账号</p>
+          <NavLink className="font-style" to="/register">注册</NavLink>
         </div>
+        <div className="login-right">
+          <h1 className="text-style-1 font-style">欢迎来到Aoblog</h1>
+          <i className="Rectangle-2"></i>
+          <p className="text_style_2 font-style">欢迎来到Aoblog！</p>
+          <div id="login">
+            <i className="user"><SmileOutlined style={{ fontSize: "27px", color: "#666" }} /></i>
+            <input type="text" name={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="请输入您的用户名" id="hs_user" />
+            <i className="password"><KeyOutlined style={{ fontSize: "27px", color: "#666" }} /></i>
+            <input type="password" name={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="请输入您的密码" id="hs_psw" />
+            <div className="info font-style">{info}</div>
+            <button onClick={handleLogin} className="btn font-style">登录</button>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
